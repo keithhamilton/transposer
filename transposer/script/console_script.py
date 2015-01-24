6d39bd075505222883a76059d0b743f0a17b4ce3
Keith Hamilton <the.keith.hamilton@gmail.com>
2015-01-24 08:00:42 -0800

    updated flags

--- transposer/script/console_script.py
+++ transposer/script/console_script.py
@@ -8,11 +8,11 @@ import transposer
 def parse_args():
     parser = argparse.ArgumentParser(
         description="command line args to assist with development")
-    parser.add_argument('--in', dest='file_in', default=None, required=True,
-                        help='file from which to read input data')
-    parser.add_argument('--out', dest='file_out', default=None, required=True,
+    parser.add_argument('-i', '--in', dest='file_in', default=None, 
+                        required=True, help='file from which to read input data')
+    parser.add_argument('-o', '--out', dest='file_out', default=None,
                         help='file to which results should be written')
-    parser.add_argument('--delimiter', dest='delimiter', default=',',
+    parser.add_argument('-d', '--delimiter', dest='delimiter', default=',',
                         help='the type of delmiter to read and write with' +
                                'defaults to \',\'')
     return parser.parse_args()
@@ -20,5 +20,4 @@ def parse_args():
 
 def main():
     args = parse_args()
-    if args.file_in and args.file_out:
-        transposer.transpose(args.file_in, args.file_out, args.delimiter)
+    transposer.transpose(i=args.file_in, o=args.file_out, d=args.delimiter)
